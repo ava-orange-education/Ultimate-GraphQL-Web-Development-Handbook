d227b4a5dadd38dae2a0f46121d3b3b83dbf70e0
Robins Gupta <robinskumar73@gmail.com>
2025-09-05 00:52:48 +0530

    Chapter 4 added

---
+++ chapter 4/backend/connections/mongo.js
@@ -0,0 +1,23 @@
+// Import necessary modules
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// MongoDB connection URL
+const MONGODB_URI = process.env.MONGODB_URI; // Use environment variable for MongoDB URI
+
+// Function to connect to MongoDB using Mongoose
+const connectToMongoDB = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(MONGODB_URI, {});
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+// Export the function to connect to MongoDB
+export default connectToMongoDB;
